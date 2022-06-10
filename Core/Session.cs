f7ab36008b3fc905eee10e9d9a45a9f0624d7b6b
pushrsp@gmail.com <pushrsp@gmail.com>
2022-06-10 17:15:31 +0900

    ADD: 우선순위 큐

--- Core/Session.cs
+++ Core/Session.cs
@@ -6,6 +6,37 @@ using System.Threading;
 
 namespace Core
 {
+    public abstract class PacketSession : Session
+    {
+        public static readonly int HeaderSize = 2;
+
+        public sealed override int OnRecv(ArraySegment<byte> buffer)
+        {
+            int processLen = 0;
+            int packetCount = 0;
+
+            while (true)
+            {
+                if (buffer.Count < HeaderSize)
+                    break;
+
+                ushort dataSize = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+                if (buffer.Count < dataSize)
+                    break;
+
+                OnRecvPacket(new ArraySegment<byte>(buffer.Array, buffer.Offset, dataSize));
+                packetCount++;
+
+                processLen += dataSize;
+                buffer = new ArraySegment<byte>(buffer.Array, buffer.Offset + dataSize, buffer.Count - dataSize);
+            }
+
+            return processLen;
+        }
+
+        public abstract void OnRecvPacket(ArraySegment<byte> buffer);
+    }
+
     public abstract class Session
     {
         private Socket _socket;
